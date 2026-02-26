5dfce1a5adcf830f0a23ac855b1189a4ca477217
Valentina BM <valentina15bermo@gmail.com>
2026-02-26 12:38:12 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// //BLOCKING, SYNCHRONOUS WAY //
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+//NON BLOCKING, ASYNCHRONOUS WAY //
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", "${data2}/n${data3}", "uft-8", (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
+console.log("Will read file!");
