37d8e46f7d6e5fe5425e55694fea587b5e8c0479
Valentina BM <valentina15bermo@gmail.com>
2026-04-17 12:46:17 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -32,9 +32,21 @@ const url = require("url");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === "/overview") {
+  if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
   }
 });
 
