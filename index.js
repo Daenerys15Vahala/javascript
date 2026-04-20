3ea528d53d1e3ff6db02729952845c49ed435c30
Valentina BM <valentina15bermo@gmail.com>
2026-04-20 12:46:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -31,19 +31,48 @@ const url = require("url");
 
 //////////////////////////////////////
 //SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace;
+};
+
+const tempOverview = fs.readFileSync(
+  "${__dirname}/templates/template-overview.html",
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  "${__dirname}/templates/template-card.html",
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  "${__dirname}/templates/template-product.html",
+  "utf-8",
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // OVERVIEW PAGE
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    res.end(tempOverview);
+
+    // PRDOCUT PAGE
   } else if (pathName === "./product") {
     res.end("This is the PRODUCT");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "applicatiion/json" });
     res.end(data);
+
+    // NOT FOUND
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
