2f2b6be0a3c514f05a1b88185dd0bbd3787c55d2
Valentina BM <valentina15bermo@gmail.com>
2026-04-22 13:39:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-require("./module");
 
-/////////////////////
-/////FILES
+const replaceTemplate = require("./modules/replaceTemplate");
 
-//Blocking, synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
-
-//Non-blocking, asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if (err) return console.log("ERROR!💥");
-
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3)};
-
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("Your file has been written☺️");
-//       });
-//     });
-//   });
-// });
-// console.log("Will read file!");
-
-////////////////////////////////////
-// SERVER
-
+// Read templates and data once at startup
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8",
@@ -50,13 +21,16 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slugify("Fresh Avocados", { lower: true }));
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
-  // OVERVIEW PAGE
+  // OVERVIEW
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-Type": "text/html" });
-
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
@@ -65,9 +39,15 @@ const server = http.createServer((req, res) => {
 
     // PRODUCT
   } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    const product = dataObj[query.id];
+    const id = query.id;
+    const product = dataObj[id];
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>Product not found</h1>");
+      return;
+    }
     const output = replaceTemplate(tempProduct, product);
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(output);
 
     // API
