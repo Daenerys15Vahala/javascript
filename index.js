be5833cf4d20734014518016af16e1ca5faeee95
Valentina BM <valentina15bermo@gmail.com>
2026-04-23 12:55:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const dataObj = JSON.parse(data);
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 console.log(slugify("Fresh Avocados", { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
