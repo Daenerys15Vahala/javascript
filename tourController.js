7e769128a7a2fb6fc4053efa1ecaaee769657173
Valentina BM <valentina15bermo@gmail.com>
2026-05-26 13:49:47 -0400

    Add files via upload

--- tourController.js
+++ tourController.js
@@ -57,7 +57,6 @@ exports.createTour = (req, res) => {
 
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
 
   fs.writeFile(
