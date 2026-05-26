2227a7b6f1f0a5ced9861495c6ab96249278ca4e
Valentina BM <valentina15bermo@gmail.com>
2026-05-26 13:50:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
