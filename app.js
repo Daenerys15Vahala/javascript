4dd241e793e0cec7a17d275931c3fb4957e9bad9
Valentina BM <valentina15bermo@gmail.com>
2026-05-26 13:25:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(morgan('dev'));
 app.use(express.json());
