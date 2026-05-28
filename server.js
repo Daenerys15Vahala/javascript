02a04cdf5145a8e124e306fd84c780716f13e2f3
Valentina BM <valentina15bermo@gmail.com>
2026-05-27 22:13:37 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -20,11 +20,19 @@ mongoose
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, ' A tour must have a name'],
+    unique: true,
   },
-  rating: Number,
-  price: Number,
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, ' A tour must have a price'],
+  },
 });
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
